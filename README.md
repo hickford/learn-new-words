04e9a74f6ed9634d034d9112440ddb44ccf61984
Matt Hickford <matt.hickford@gmail.com>
2014-10-04 15:40:11 +0100

    lemmatize

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 learn-new-words
 ===============
+
+Given a book, find novel words in it.
+
+Prerequisites
+----
+
+1. `dictionary.txt` a list of English dictionary words
+2. `common.txt` a list of common English words
+
+Usage
+----
+
+    python learn-new-words.py book.txt
+
+Assumes the text file is utf-8 encoded.
--- learn-new-words.py
+++ learn-new-words.py
@@ -12,18 +12,44 @@ def words():
 dictionary = set(open('dictionary.txt').read().split())
 commons = set(open('common.txt').read().split())
 
-def is_rare(word):
-    return word in dictionary and word not in commons
+# lemmatizers
+porter = nltk.stem.PorterStemmer()
+lancaster = nltk.stem.LancasterStemmer()
+snowball = nltk.stem.snowball.EnglishStemmer()
+wordnet = nltk.stem.WordNetLemmatizer() # slow
 
+def is_common(word):
+    if word in commons:
+        return True
+    if porter.stem(word) in commons:
+        return True
+    if lancaster.stem(word) in commons:
+        return True
+    if snowball.stem(word) in commons:
+        return True
+    if wordnet.lemmatize(word) in commons:
+        return True
+    if wordnet.lemmatize(word, 'v') in commons:
+        return True
 
 found = set()
 
 for word in words():
-    word = word.lower()
+    if not word.isalpha():
+        continue
+
+    if not word.islower():
+        continue
+
+    if word not in dictionary:
+        # probably a name
+        continue
 
     if word in found:
         continue
 
-    if is_rare(word):
-        found.add(word)
-        print(word)
+    if is_common(word):
+        continue
+
+    found.add(word)
+    print(word)
